360e9c9a19d0c553fba686a5079370d568714645
johan guillen <sioupes34@gmail.com>
2024-10-21 17:54:54 +0200

    tp03 SQL

---
+++ tp03_guillen.sql
@@ -0,0 +1,24 @@
+ALTER TABLE article ADD CONSTRAINT FOREIGN KEY (ID_FOU) REFERENCES fournisseur (ID);
+ALTER TABLE bon ADD CONSTRAINT FOREIGN KEY (ID_FOU) REFERENCES fournisseur (ID);
+ALTER TABLE compo ADD CONSTRAINT FOREIGN KEY (ID_ART) REFERENCES article (ID);
+ALTER TABLE compo ADD CONSTRAINT FOREIGN KEY (ID_BON) REFERENCES bon (ID);
+
+INSERT INTO fournisseur (ID,NOM)
+ values
+(1,"Francaise d\'imports"),
+(2,"FDM SA"),
+(3,"Dubois & Fils");
+
+
+INSERT INTO article (ID,REF,DESIGNATION,PRIX,ID_FOU)
+ values
+(1,"A01","Perceuse P1",74.99,1),
+(2,"F01","Boulon laiton 4 x 40 mm (sachet de 10)",2.25,2),
+(3,"F02","Boulon laiton 5 x 40 mm (sachet de 10)",4.45,2),
+(4,"D01","Boulon laiton 5 x 40 mm (sachet de 10)",4.40,3),
+(5,"A02","Meuleuse 125mm",37.85,1),
+(6,"D03","Boulon acier zingué 4 x 40mm (sachet de 10)",1.8,3),
+(7,"A03","Perceuse à colonne",185.25,1),
+(8,"D04","Coffret mêches à bois",12.25,3),
+(9,"F03","Coffret mêches plates",6.25,2),
+(10,"F04","Fraises d’encastrement",8.14,2);
